7837471ce564ffc2f18cd7f33277288657a9581b
vagrant <vagrant@localhost.localdomain>
2020-08-03 13:24:21 +0000

    4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,6 +30,17 @@ class UsersController < ApplicationController
       render "users/edit"
     end
   end
+  def following
+      @user  = User.find(params[:id])
+      @users = @user.followings
+      render 'show_follow'
+  end
+
+  def followers
+    @user  = User.find(params[:id])
+    @users = @user.followers
+    render 'show_follower'
+  end
 
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,16 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about'
   get 'users/mypage' => 'users#index'
 
-  resources :users, only: [:show, :index, :create, :update, :edit]
+  resources :users, only: [:show, :index, :create, :update, :edit] do
+  	member do
+  		get :following, :followers
+  	end
+  end
+  resources :relationships, only: [:create, :destroy]
   resources :books, only: [:edit, :create, :update, :index, :show, :destroy] do
-	  resource :favorites, only: [:create, :destroy]
-	  resources :book_comments, only: [:create, :destroy]
+  resource :favorites, only: [:create, :destroy]
+  resources :book_comments, only: [:create, :destroy]
+
  end
 
 end
