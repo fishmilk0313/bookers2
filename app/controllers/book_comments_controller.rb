740da7687aec61b25e2d9984d05535a341fa787e
vagrant <vagrant@localhost.localdomain>
2020-08-05 13:40:29 +0000

    課題6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,16 +1,16 @@
 class BookCommentsController < ApplicationController
   def create
-  	book = Book.find(params[:book_id])
-  	comment = current_user.book_comments.new(book_comment_params)
-  	comment.book_id = book.id
-  	comment.save
-  	redirect_to book_path(book)
+  	@book_show = Book.find(params[:book_id])
+  	@book_comment = current_user.book_comments.build(book_comment_params)
+  	@book_comment.book_id = @book_show.id
+    @book_comment.save
   end
 
   def destroy
-    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to book_path(params[:book_id])
-    end
+    @book_show = Book.find(params[:book_id])
+    @book_comment = BookComment.find_by(id: params[:id], book_id: params[:book_id])
+    @book_comment.destroy
+  end
 
   private
   def book_comment_params
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,13 +4,11 @@ class FavoritesController < ApplicationController
   	# favorite = current_user.favorites.new(book_id: @book.id)
   	favorite = @book.favorites.new(user_id: current_user.id)
   	favorite.save
-  	redirect_to books_path(@book)
   end
 
   def destroy
   	@book = Book.find(params[:book_id])
   	favorite = current_user.favorites.find_by(book_id: @book.id)
   	favorite.destroy
-  	redirect_to books_path(@book)
   end
 end
